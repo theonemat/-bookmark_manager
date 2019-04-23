d9be44520c6e0725dbbb9e327db01a1f9676e78f
mat <matmoon14@gmail.com>
2019-04-23 16:35:50 +0100

    addd sql database and shotgun

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
-require 'sinatra'
+require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Hello World'
   end
 
-get '/bookmark' do
-  
-end
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end
 
   run! if app_file == $0
 end
