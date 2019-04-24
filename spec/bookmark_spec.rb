b8c17251d2fcbb867ea2eede2b917bd39505f938
mat <matmoon14@gmail.com>
2019-04-25 00:48:13 +0100

    sql database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,12 +1,12 @@
 require 'bookmark'
 
-describe Bookmark do
-  describe '.all' do
-    it 'returns all bookmarks' do
-        bookmarks = Bookmark.all
-        expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
-    end
+
+describe '.all' do
+  it 'returns a list of bookmarks' do
+    bookmarks = Bookmark.all
+
+    expect(bookmarks).to include "http://www.makersacademy.com"
+    expect(bookmarks).to include "http://www.destroyallsoftware.com"
+    expect(bookmarks).to include "http://www.google.com"
   end
 end
